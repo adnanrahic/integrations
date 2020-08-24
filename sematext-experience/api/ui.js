c4e6e8217c8707006ddb864fc0dc47f600eff8d1
Adnan Rahic <ado.raha198@gmail.com>
2020-08-24 21:12:37 +0200

    initial work on creating sematext experience integration

---
+++ sematext-experience/api/ui.js
@@ -0,0 +1,13 @@
+const { withUiHook } = require('@zeit/integration-utils')
+const route = require('../lib/route')
+
+module.exports = withUiHook(async arg => {
+  let {
+    payload: { action }
+  } = arg
+  if (action === 'view') {
+    action = 'index'
+  }
+
+  return route(arg, action)
+})
